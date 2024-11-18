43f9bc4e1dea6712b8ec7ebf78f099cd25b4c68c
Akhna <akhnasj1612@gmail.com>
2024-11-19 00:11:10 +0530

    ImageView &

---
+++ FirstApplication/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.firstapplication;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void send(View view){
+        Toast.makeText(this, "Sending data from app...", Toast.LENGTH_SHORT).show();
+    }
+    public void receive(View view){
+        Toast.makeText(this, "Receiving data to app...", Toast.LENGTH_SHORT).show();
+    }
+    public void delete(View view){
+        Toast.makeText(this, "Deleting data from app...", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
