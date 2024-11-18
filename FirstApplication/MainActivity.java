612d67f4f30333b75c62dcca9a111ec9fcda90d5
Akhna <akhnasj1612@gmail.com>
2024-11-19 00:31:46 +0530

    ImageView & Send, Receive, Delete Button

--- FirstApplication/MainActivity.java
+++ FirstApplication/MainActivity.java
@@ -8,7 +8,7 @@ import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsCompat; 
 
 public class MainActivity extends AppCompatActivity {
 
